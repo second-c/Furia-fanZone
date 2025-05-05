8a92fa16536345828093ef6d60442a97ad7bcb2e
Cledyson <cledysonklinger19@gmail.com>
2025-05-05 00:47:14 -0400

    .

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_DATABASE_URL,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyDHHAJY86r8dWXPLTDpsD9XuPBCwv5QXug",
+  authDomain: "furia-fanzone-6b784.firebaseapp.com",
+  databaseURL: "https://furia-fanzone-6b784-default-rtdb.firebaseio.com",
+  projectId: "furia-fanzone-6b784",
+  storageBucket: "furia-fanzone-6b784.firebasestorage.app",
+  messagingSenderId: "168773910247",
+  appId: "1:168773910247:web:ed66fcf1b8c53ef0146c42",
 };
 
 const app = initializeApp(firebaseConfig);
